712f392841b92808fe9e5901fe93fd6d5dacce8f
Rahul <rahul.shinde@digitas.com>
2020-05-31 17:21:00 +0530

    Schema setup

---
+++ src/models/restapiModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const contactSchema = new Schema({
+    firstName: {
+        type: String,
+        required: 'Enter a First name'
+    },
+    lastName: {
+        type: String,
+        required: 'Enter a Last name'
+    },
+    email: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    phone: {
+        type: Number
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+})
