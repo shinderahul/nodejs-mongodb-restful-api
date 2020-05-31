577d380399fb380f198033a9c20668355e43eac6
Rahul <rahul.shinde@digitas.com>
2020-05-31 17:12:33 +0530

    mongodb setup

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
-import routes from './src/routes/restapiRoutes'
+import routes from './src/routes/restapiRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser'
 
 const app = express();
-const PORT = 4000;
+const PORT = 5000;
+
+// mongoose connection
+mongoose.connect('mongodb://localhost/restapiDb', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
 
 routes(app)
 
