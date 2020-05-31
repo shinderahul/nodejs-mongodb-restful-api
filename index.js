2ed766f2561bf77ed602f3252ac27cb12f1387c8
Rahul <rahul.shinde@digitas.com>
2020-05-31 16:54:55 +0530

    basic routing endpoint

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/restapiRoutes'
 
 const app = express();
 const PORT = 4000;
+
+routes(app)
 
 app.get('/', (req, res) => {
     res.send(`Server running on port ${PORT}`)
